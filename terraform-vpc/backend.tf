84b40bab11a9ed43c67784bfdedb5f24bfc7e46d
Bsindhujareddy <sindhuja49b@gmail.com>
2026-04-21 00:20:15 +0530

    fc

--- terraform-vpc/backend.tf
+++ terraform-vpc/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "nikbsoft-terraform-lock-table"   # your S3 bucket
+    bucket         = "nikbsoft-terraform-state-bucket"   # your S3 bucket
     key            = "vpc-sample/terraform.tfstate"             # path inside bucket
     region         = "us-east-1"
-    dynamodb_table = "nikbsoft-terraform-state-bucket"
+    dynamodb_table = "nikbsoft-terraform-lock-table"
     encrypt        = true
   }
 }
